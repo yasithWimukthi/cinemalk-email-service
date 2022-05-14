bf3308558a51da1947504b95fd6dd3aa6f7e0033
Chamodh Dasanayake <78280086+Cham-88D@users.noreply.github.com>
2022-05-15 02:16:31 +0530

    Updated cors

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import routes from './routes/email_route.js';
 import dotenv from 'dotenv'
-
+import cors from 'cors'
 dotenv.config();
 
 const port = process.env.MAIL_PORT;
 const app = new express();
-
+app.use(cors())
 app.use(express.json());
+
 app.use('/api/v1/',routes);
 
 app.listen(port, () => { console.log(`server started - listening on port ${port}`)})
